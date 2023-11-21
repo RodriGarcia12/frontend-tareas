4490e7bb85ebf948350bddcbc84fda3afa1aae86
Rodrigo <rgarciaa125@gmail.com>
2023-11-21 00:39:01 -0300

    se agregan rutas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/createTask", function () {
+    return view("createTask");
+});
+Route::post("/createTask",[TaskController::class,"Create"]);
+
+Route::get("/tasks",[TaskController::class,"Read"]);
+Route::get("/task/{d}",[TaskController::class,"GetTask"]);
+Route::get("/deleteTask/{d}",[TaskController::class,'Delete']);
